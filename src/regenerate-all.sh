a7a017f9830284300a4cc1262072540b43043283
Vuenc <v.buergin@gmx.de>
2023-04-07 11:59:07 +0200

    Added regenerate script

---
+++ src/regenerate-all.sh
@@ -0,0 +1,8 @@
+# Informatics
+python fetch_tumonline_tree.py --curriculum master-informatics
+python print_html_table.py --termid 198 --curriculum master-informatics --output ../informatics-ss23.html
+python print_html_table.py --termid 198 --curriculum master-informatics --oldtermsfrom 188 --output ../informatics-all.html
+# DEA
+python fetch_tumonline_tree.py --curriculum master-dea
+python print_html_table.py --termid 198 --curriculum master-dea --output ../dea-ss23.html
+python print_html_table.py --termid 198 --curriculum master-dea --oldtermsfrom 188 --output ../dea-all.html
