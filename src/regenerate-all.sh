d9e2c5fc0898ff2580a8f6072fd7d980e3370fd6
Vuenc <v.buergin@gmx.de>
2023-08-02 13:11:33 +0200

    Updated regeneration bash script

--- src/regenerate-all.sh
+++ src/regenerate-all.sh
@@ -1,8 +1,12 @@
 # Informatics
+TERMID=199
+TERMNAME=ws23-24
+FIRSTTERMID=171 # no data before WS09/10
+
 python fetch_tumonline_tree.py --curriculum master-informatics
-python print_html_table.py --termid 198 --curriculum master-informatics --output ../informatics-ss23.html
-python print_html_table.py --termid 198 --curriculum master-informatics --oldtermsfrom 188 --output ../informatics-all.html
+python print_html_table.py --termid $TERMID --curriculum master-informatics --output "../informatics-$TERMNAME.html"
+python print_html_table.py --termid $TERMID --curriculum master-informatics --oldtermsfrom $FIRSTTERMID --output ../informatics-all.html
 # DEA
 python fetch_tumonline_tree.py --curriculum master-dea
-python print_html_table.py --termid 198 --curriculum master-dea --output ../dea-ss23.html
-python print_html_table.py --termid 198 --curriculum master-dea --oldtermsfrom 188 --output ../dea-all.html
+python print_html_table.py --termid $TERMID --curriculum master-dea --output "../dea-$TERMNAME.html"
+python print_html_table.py --termid $TERMID --curriculum master-dea --oldtermsfrom $FIRSTTERMID --output ../dea-all.html
