5e35370860a5cf1cd086350459e9a8b58983603c
Vincent Bürgin <vincent.buergin@tum.de>
2024-09-10 14:55:36 +0200

    Updated fetch_offered_courses.py to use cached course info for old semesters if available

--- src/fetch_offered_courses.py
+++ src/fetch_offered_courses.py
@@ -4,6 +4,7 @@ import requests
 import re
 import argparse
 import multiprocessing.pool
+import os
 
 import tqdm.contrib.concurrent
 import util
@@ -66,60 +67,59 @@ def fetch_courses_for_term(term_id_and_name):
         course_dto["title"] = next((t["value"] for t in course_dto["courseTitle"]["translations"]["translation"] if t["lang"] == "en" and "value" in t), course_dto["courseTitle"]["value"])
     return term_course_dtos
 
+def clean_dto(dto, fields):
+    out = {}
+    for field, field_selector in fields.items():
+        if isinstance(field_selector, dict):
+            out[field] = clean_dto(dto[field], field_selector)
+        else:
+            out[field] = dto[field]
+    return out
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(usage="fetch_offered_courses.py [-h] --termid TERMID\n\nPlease provide the term id: winter 2022/23 is 197, summer 2023 is 198, winter 2023/24 is 199, etc.")
-    parser.add_argument('--termid', required=True, type=int, help="The term id (winter 2022/23 is 197, summer 2023 is 198, etc.)")
+    parser = argparse.ArgumentParser(
+        usage="""
+        fetch_offered_courses.py [-h] --termid TERMID [--oldtermsfrom OLDTERMSFROMID] [--jsonpath JSONPATH]
+        
+        Please provide the term id: winter 2023/24 is 199, summer 2024 is 200, winter 2024/25 is 203 (!), etc.
+        """)
+    parser.add_argument('--termid', required=True, type=int, help="The term id (winter 2023/24 is 199, summer 2024 is 200, winter 2024/25 is 203 (!), etc)")
     parser.add_argument('--oldtermsfrom', required=False, type=int, help="The first included term when fetching courses for a range of terms (in this case, --termid specifies the last one)")
+    parser.add_argument('--jsonpath', required=False, type=str, default="../data/all_offered_courses.json", help="Path where the json file with all courses is stored. If it exists, courses for old terms are fetched from there instead of being downloaded. Default: '../data/all_offered_courses.json'")
     args = parser.parse_args()
 
-    terms = [(term_id, util.term_id_to_name(term_id)) for term_id in range(args.oldtermsfrom if args.oldtermsfrom is not None else args.termid, args.termid+1) if term_id not in [201, 202]]
+    existing_data = []
+    existing_terms = []
+    if os.path.isfile(args.jsonpath):
+        with open(args.jsonpath) as f:
+            existing_data = json.load(f)
+            # Remove existing entries for current semester: this should always be fetched (this is likely to have changed)
+            existing_data = [dto for dto in existing_data if dto["semesterDto"]["id"] != args.termid]
+        existing_terms = list(set(dto["semesterDto"]["id"] for dto in existing_data).difference({args.termid}))
+        print(f"Found data for old terms in '{args.jsonpath}'. This data is not downloaded again.")
+        print(f"Old terms found: {', '.join(util.term_id_to_name(term_id) for term_id in sorted(existing_terms))}" )
+
+    terms = [(term_id, util.term_id_to_name(term_id))
+             for term_id in range(args.oldtermsfrom if args.oldtermsfrom is not None else args.termid, args.termid+1)
+             if term_id not in [201, 202]
+             and term_id not in existing_terms
+    ]
 
     print(f"Fetching offered courses from {terms[0][1]} to {terms[-1][1]}")
     # all offered courses are stored from newest to oldest
     available_courses_dtos = sum(
         tqdm.contrib.concurrent.thread_map(
-            fetch_courses_for_term, reversed(terms), max_workers=len(terms)),
+            # Enable parallelism by setting max_workers > 1 (e.g. `len(terms)`). Warning: might be too many HTTP requests to handle.
+            fetch_courses_for_term, reversed(terms), max_workers=1 # len(terms)
+        ),
         []
     )
 
-    with open("../data/all_offered_courses.json", "w") as f:
-        json.dump(available_courses_dtos, f)
+    # Clean the retrieved DTOs (removed unneeded fields) to save some space and json parsing time
+    field_selector = {"courseTypeDto": {"key": True}, "termName": True, "semesterDto": {"id": True}, "title": True, "id": True,}
+    available_courses_dtos = list(map(lambda d: clean_dto(d, field_selector), available_courses_dtos))
 
-    print("Results written to JSON file ../data/all_offered_courses.json")
+    with open(args.jsonpath, "w") as f:
+        json.dump(existing_data + available_courses_dtos, f)
 
-    # if args.termid % 2 == 0:
-    #     print(f"Term ID {args.termid}: Summer term {int(1924 + args.termid/2)}")
-    # else:
-    #     print(f"Term ID {args.termid}: Winter term {int(1924 + args.termid/2)}/{int(1924 + args.termid/2)+1}")
-
-    # print("Searching for courses associated with Master informatics curriculum...")
-    # courses = fetch_course_dtos(term_id=args.termid, only_master_informatics=True)
-    # course_INs_names = extract_course_INs_names(courses)
-
-    # course_INs_names = sorted(course_INs_names, key=lambda c: c[1])
-
-    # print("VO/VI courses associated with Master informatics curriculum:")
-    # print()
-    # for (_, IN) in course_INs_names:
-    #     print(IN)
-    # for (name, _) in course_INs_names:
-    #     print(name)
-
-
-    # print()
-    # print("##############################################################################")
-    # print()
-    # print("Searching for other courses with IN number...")
-    # courses_all = fetch_course_dtos(term_id=args.termid, only_master_informatics=False)
-    # course_INs_names_all = extract_course_INs_names(courses_all)
-
-    # course_INs_names_all = set(course_INs_names_all) - set(course_INs_names)
-
-    # course_INs_names_all = sorted(course_INs_names_all, key=lambda c: c[1])
-    
-
-    # print("VO/VI courses with IN number *not* associated with Master informatics curriculum, according to TUM online:")
-    # for (_, IN) in course_INs_names_all:
-    #     print(IN)
-    # for (name, _) in course_INs_names_all:
-    #     print(name)
+    print(f"Results written to JSON file '{args.jsonpath}'")
