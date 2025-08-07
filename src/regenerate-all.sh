98065bade870a13429d25d228bc3c35ce0a09415
Vincent Bürgin <vincent.buergin@tum.de>
2025-08-07 08:52:36 +0200

    Updated README.md and regenerate-all.sh

--- src/regenerate-all.sh
+++ src/regenerate-all.sh
@@ -2,20 +2,15 @@ TERMID=204
 TERMNAME=ss25
 FIRSTTERMID=171 # no data before WS09/10
 
-python fetch_offered_courses.py --termid $TERMID --oldtermsfrom $FIRSTTERMID
 
 # Informatics
-python fetch_curriculum_tree.py --curriculum master-informatics
+python update_course_database.py --termid $TERMID --oldtermsfrom $FIRSTTERMID --curriculum master-informatics
+python fetch_curriculum_tree.py --parallel_drivers 5 --curriculum master-informatics
 python print_html_table.py --termid $TERMID --curriculum master-informatics --output "../informatics-$TERMNAME.html"
-python print_html_table.py --termid $TERMID --curriculum master-informatics --oldtermsfrom $FIRSTTERMID --output ../informatics-all.html
+python print_html_table.py --termid $TERMID --oldtermsfrom $FIRSTTERMID --curriculum master-informatics --output ../informatics-all.html
 
 # DEA
-python fetch_curriculum_tree.py --curriculum master-dea
+python update_course_database.py --termid $TERMID --oldtermsfrom $FIRSTTERMID --curriculum master-dea
+python fetch_curriculum_tree.py --parallel_drivers 5 --curriculum master-dea
 python print_html_table.py --termid $TERMID --curriculum master-dea --output "../dea-$TERMNAME.html"
-python print_html_table.py --termid $TERMID --curriculum master-dea --oldtermsfrom $FIRSTTERMID --output ../dea-all.html
-
-# Informatics, FSPO 2023
-python fetch_curriculum_tree.py --curriculum master-informatics-fspo2023
-python print_html_table.py --termid $TERMID --curriculum master-informatics-fspo2023 --output "../informatics-fspo2023-$TERMNAME.html"
-python print_html_table.py --termid $TERMID --curriculum master-informatics-fspo2023 --oldtermsfrom $FIRSTTERMID --output ../informatics-fspo2023-all.html
-
+python print_html_table.py --termid $TERMID --oldtermsfrom $FIRSTTERMID --curriculum master-dea --output ../dea-all.html
